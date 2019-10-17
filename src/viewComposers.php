07568ef968071ee56b93398b2e4b32de55cad6e4
opic <floresopic@gmail.com>
2019-10-17 20:14:04 +0800

    chore(url routing): revert back to frozennode. conflict with subdirectory routing

--- src/viewComposers.php
+++ src/viewComposers.php
@@ -3,7 +3,8 @@
 use Illuminate\Support\Facades\View;
 
 //admin index view
-View::composer('administrator::index', function ($view) {
+View::composer('administrator::index', function($view)
+{
 	//get a model instance that we'll use for constructing stuff
 	$config = app('itemconfig');
 	$fieldFactory = app('admin_field_factory');
@@ -11,7 +12,7 @@ View::composer('administrator::index', function ($view) {
 	$actionFactory = app('admin_action_factory');
 	$dataTable = app('admin_datatable');
 	$model = $config->getDataModel();
-	$baseUrl = route('admin_dashboard', array(), false);
+	$baseUrl = route('admin_dashboard');
 	$route = parse_url($baseUrl);
 
 	//add the view fields
@@ -30,16 +31,17 @@ View::composer('administrator::index', function ($view) {
 	$view->formWidth = $config->getOption('form_width');
 	$view->baseUrl = $baseUrl;
 	$view->assetUrl = url('packages/frozennode/administrator/');
-	$view->route = $route['path'] . '/';
-	$view->itemId = $view->itemId ?? null;
+	$view->route = $route['path'].'/';
+	$view->itemId = isset($view->itemId) ? $view->itemId : null;
 });
 
 //admin settings view
-View::composer('administrator::settings', function ($view) {
+View::composer('administrator::settings', function($view)
+{
 	$config = app('itemconfig');
 	$fieldFactory = app('admin_field_factory');
 	$actionFactory = app('admin_action_factory');
-	$baseUrl = route('admin_dashboard', array(), false);
+	$baseUrl = route('admin_dashboard');
 	$route = parse_url($baseUrl);
 
 	//add the view fields
@@ -49,84 +51,85 @@ View::composer('administrator::settings', function ($view) {
 	$view->actions = $actionFactory->getActionsOptions();
 	$view->baseUrl = $baseUrl;
 	$view->assetUrl = url('packages/frozennode/administrator/');
-	$view->route = $route['path'] . '/';
+	$view->route = $route['path'].'/';
 });
 
 //header view
-View::composer(array('administrator::partials.header'), function ($view) {
+View::composer(array('administrator::partials.header'), function($view)
+{
 	$view->menu = app('admin_menu')->getMenu();
 	$view->settingsPrefix = app('admin_config_factory')->getSettingsPrefix();
 	$view->pagePrefix = app('admin_config_factory')->getPagePrefix();
-	$view->routePrefix = app('admin_config_factory')->getRoutePrefix();
 	$view->configType = app()->bound('itemconfig') ? app('itemconfig')->getType() : false;
 });
 
 //the layout view
-View::composer(array('administrator::layouts.default'), function ($view) {
-
-	$secureAssets = config('administrator.force_https_assets') ? true : null;
-
+View::composer(array('administrator::layouts.default'), function($view)
+{
 	//set up the basic asset arrays
 	$view->css = array();
 	$view->js = array(
-		'jquery' => asset('packages/frozennode/administrator/js/jquery/jquery-1.8.2.min.js', $secureAssets),
-		'jquery-ui' => asset('packages/frozennode/administrator/js/jquery/jquery-ui-1.10.3.custom.min.js', $secureAssets),
-		'customscroll' => asset('packages/frozennode/administrator/js/jquery/customscroll/jquery.customscroll.js', $secureAssets),
+		'jquery' => asset('packages/frozennode/administrator/js/jquery/jquery-1.8.2.min.js'),
+		'jquery-ui' => asset('packages/frozennode/administrator/js/jquery/jquery-ui-1.10.3.custom.min.js'),
+		'customscroll' => asset('packages/frozennode/administrator/js/jquery/customscroll/jquery.customscroll.js'),
 	);
 
 	//add the non-custom-page css assets
-	if (!$view->page && !$view->dashboard) {
+	if (!$view->page && !$view->dashboard)
+	{
 		$view->css += array(
-			'jquery-ui' => asset('packages/frozennode/administrator/css/ui/jquery-ui-1.9.1.custom.min.css', $secureAssets),
-			'jquery-ui-timepicker' => asset('packages/frozennode/administrator/css/ui/jquery.ui.timepicker.css', $secureAssets),
-			'select2' => asset('packages/frozennode/administrator/js/jquery/select2/select2.css', $secureAssets),
-			'jquery-colorpicker' => asset('packages/frozennode/administrator/css/jquery.lw-colorpicker.css', $secureAssets),
+			'jquery-ui' => asset('packages/frozennode/administrator/css/ui/jquery-ui-1.9.1.custom.min.css'),
+			'jquery-ui-timepicker' => asset('packages/frozennode/administrator/css/ui/jquery.ui.timepicker.css'),
+			'select2' => asset('packages/frozennode/administrator/js/jquery/select2/select2.css'),
+			'jquery-colorpicker' => asset('packages/frozennode/administrator/css/jquery.lw-colorpicker.css'),
 		);
 	}
 
 	//add the package-wide css assets
 	$view->css += array(
-		'customscroll' => asset('packages/frozennode/administrator/js/jquery/customscroll/customscroll.css', $secureAssets),
-		'main' => asset('packages/frozennode/administrator/css/main.css', $secureAssets),
+		'customscroll' => asset('packages/frozennode/administrator/js/jquery/customscroll/customscroll.css'),
+		'main' => asset('packages/frozennode/administrator/css/main.css'),
 	);
 
 	//add the non-custom-page js assets
-	if (!$view->page && !$view->dashboard) {
+	if (!$view->page && !$view->dashboard)
+	{
 		$view->js += array(
-			'select2' => asset('packages/frozennode/administrator/js/jquery/select2/select2.js', $secureAssets),
-			'jquery-ui-timepicker' => asset('packages/frozennode/administrator/js/jquery/jquery-ui-timepicker-addon.js', $secureAssets),
-			'ckeditor' => asset('packages/frozennode/administrator/js/ckeditor/ckeditor.js', $secureAssets),
-			'ckeditor-jquery' => asset('packages/frozennode/administrator/js/ckeditor/adapters/jquery.js', $secureAssets),
-			'markdown' => asset('packages/frozennode/administrator/js/markdown.js', $secureAssets),
-			'plupload' => asset('packages/frozennode/administrator/js/plupload/js/plupload.full.js', $secureAssets),
+			'select2' => asset('packages/frozennode/administrator/js/jquery/select2/select2.js'),
+			'jquery-ui-timepicker' => asset('packages/frozennode/administrator/js/jquery/jquery-ui-timepicker-addon.js'),
+			'ckeditor' => asset('packages/frozennode/administrator/js/ckeditor/ckeditor.js'),
+			'ckeditor-jquery' => asset('packages/frozennode/administrator/js/ckeditor/adapters/jquery.js'),
+			'markdown' => asset('packages/frozennode/administrator/js/markdown.js'),
+			'plupload' => asset('packages/frozennode/administrator/js/plupload/js/plupload.full.js'),
 		);
 
 		//localization js assets
 		$locale = config('app.locale');
 
-		if ($locale !== 'en') {
+		if ($locale !== 'en')
+		{
 			$view->js += array(
-				'plupload-l18n' => asset('packages/frozennode/administrator/js/plupload/js/i18n/' . $locale . '.js', $secureAssets),
-				'timepicker-l18n' => asset('packages/frozennode/administrator/js/jquery/localization/jquery-ui-timepicker-' . $locale . '.js', $secureAssets),
-				'datepicker-l18n' => asset('packages/frozennode/administrator/js/jquery/i18n/jquery.ui.datepicker-' . $locale . '.js', $secureAssets),
-				'select2-l18n' => asset('packages/frozennode/administrator/js/jquery/select2/select2_locale_' . $locale . '.js', $secureAssets),
+				'plupload-l18n' => asset('packages/frozennode/administrator/js/plupload/js/i18n/'.$locale.'.js'),
+				'timepicker-l18n' => asset('packages/frozennode/administrator/js/jquery/localization/jquery-ui-timepicker-'.$locale.'.js'),
+				'datepicker-l18n' => asset('packages/frozennode/administrator/js/jquery/i18n/jquery.ui.datepicker-'.$locale.'.js'),
+				'select2-l18n' => asset('packages/frozennode/administrator/js/jquery/select2/select2_locale_'.$locale.'.js'),
 			);
 		}
 
 		//remaining js assets
 		$view->js += array(
-			'knockout' => asset('packages/frozennode/administrator/js/knockout/knockout-2.2.0.js', $secureAssets),
-			'knockout-mapping' => asset('packages/frozennode/administrator/js/knockout/knockout.mapping.js', $secureAssets),
-			'knockout-notification' => asset('packages/frozennode/administrator/js/knockout/KnockoutNotification.knockout.min.js', $secureAssets),
-			'knockout-update-data' => asset('packages/frozennode/administrator/js/knockout/knockout.updateData.js', $secureAssets),
-			'knockout-custom-bindings' => asset('packages/frozennode/administrator/js/knockout/custom-bindings.js', $secureAssets),
-			'accounting' => asset('packages/frozennode/administrator/js/accounting.js', $secureAssets),
-			'colorpicker' => asset('packages/frozennode/administrator/js/jquery/jquery.lw-colorpicker.min.js', $secureAssets),
-			'history' => asset('packages/frozennode/administrator/js/history/native.history.js', $secureAssets),
-			'admin' => asset('packages/frozennode/administrator/js/admin.js', $secureAssets),
-			'settings' => asset('packages/frozennode/administrator/js/settings.js', $secureAssets),
+			'knockout' => asset('packages/frozennode/administrator/js/knockout/knockout-2.2.0.js'),
+			'knockout-mapping' => asset('packages/frozennode/administrator/js/knockout/knockout.mapping.js'),
+			'knockout-notification' => asset('packages/frozennode/administrator/js/knockout/KnockoutNotification.knockout.min.js'),
+			'knockout-update-data' => asset('packages/frozennode/administrator/js/knockout/knockout.updateData.js'),
+			'knockout-custom-bindings' => asset('packages/frozennode/administrator/js/knockout/custom-bindings.js'),
+			'accounting' => asset('packages/frozennode/administrator/js/accounting.js'),
+			'colorpicker' => asset('packages/frozennode/administrator/js/jquery/jquery.lw-colorpicker.min.js'),
+			'history' => asset('packages/frozennode/administrator/js/history/native.history.js'),
+			'admin' => asset('packages/frozennode/administrator/js/admin.js'),
+			'settings' => asset('packages/frozennode/administrator/js/settings.js'),
 		);
 	}
 
-	$view->js += array('page' => asset('packages/frozennode/administrator/js/page.js', $secureAssets));
+	$view->js += array('page' => asset('packages/frozennode/administrator/js/page.js'));
 });
--- src/views/partials/menu_item.blade.php
+++ src/views/partials/menu_item.blade.php
@@ -7,8 +7,7 @@
 					'item' => $subitem,
 					'key' => $k,
 					'settingsPrefix' => $settingsPrefix,
-					'pagePrefix' => $pagePrefix,
-					'routePrefix' => $routePrefix,
+					'pagePrefix' => $pagePrefix
 				))?>
 			@endforeach
 		</ul>
@@ -16,13 +15,11 @@
 @else
 	<li class="item">
 		@if (strpos($key, $settingsPrefix) === 0)
-			<a href="{{route('admin_settings', array(substr($key, strlen($settingsPrefix)), false))}}">{{$item}}</a>
+			<a href="{{route('admin_settings', array(substr($key, strlen($settingsPrefix))))}}">{{$item}}</a>
 		@elseif (strpos($key, $pagePrefix) === 0)
-			<a href="{{route('admin_page', array(substr($key, strlen($pagePrefix)), false))}}">{{$item}}</a>
-		@elseif (strpos($key, $routePrefix) === 0)
-			<a href="{{route(substr($key, strlen($routePrefix)), [], false)}}">{{$item}}</a>
+			<a href="{{route('admin_page', array(substr($key, strlen($pagePrefix))))}}">{{$item}}</a>
 		@else
-			<a href="{{route('admin_index', array($key), false)}}">{{$item}}</a>
+			<a href="{{route('admin_index', array($key))}}">{{$item}}</a>
 		@endif
 	</li>
 @endif
